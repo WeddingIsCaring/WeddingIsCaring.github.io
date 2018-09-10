b5175d8475beafdd32e4f0525cdd0426986d09cf
Avdean <34350184+avdean@users.noreply.github.com>
2018-09-10 15:57:02 +0200

    Update init.js

--- es/js/init.js
+++ es/js/init.js
@@ -10,18 +10,18 @@
     $('.tooltipped').tooltip();
     $('input.autocomplete').autocomplete({
       data: {
-        "Meat": null,
-        "Fish": null,
-        "Vegetarian" : null,
-        "Meat and Meat" : null,
-        "Meat and Fish" : null,
-        "Meat and Vegetarian" : null,
-        "Fish and Meat" : null,
-        "Fish and Fish" : null,
-        "Fish and Vegetarian" : null,
-        "Vegetarian and Fish": null,
-        "Vegetarian and Meat": null,
-        "Vegetarian and Vegetarian" : null,
+        "Carne": null,
+        "Pescado": null,
+        "Vegetariano" : null,
+        "Carne y Carne" : null,
+        "Carne y Pescado" : null,
+        "Carne y Vegetariano" : null,
+        "Pescado y Carne" : null,
+        "Pescado y Pescado" : null,
+        "Pescado y Vegetariano" : null,
+        "Vegetariano y Pescado": null,
+        "Vegetariano y Carne": null,
+        "Vegetariano y Vegetariano" : null,
       },
     });
   }); // end of document ready
